0ddf0b3c586abc575734e3aa1cb4e16eb83cf135
mateusfelixdias <felixmateus618@gmail.com>
2022-11-09 16:26:52 -0300

    config: connection to mongodB

---
+++ src/database/connection.ts
@@ -0,0 +1,9 @@
+import { connect } from "mongoose";
+
+export function connection() {
+  const connectMongodb = connect("mongodb://localhost:27017/crud", (err) => {
+    if (err) return console.log(err);
+  });
+
+  return connectMongodb;
+};
