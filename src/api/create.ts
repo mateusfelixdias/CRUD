aa4852989d934ff133e605686c98006d4e717385
mateusfelixdias <felixmateus618@gmail.com>
2022-11-09 16:29:19 -0300

    feat: added creating a user

---
+++ src/api/create.ts
@@ -0,0 +1,22 @@
+import { Response } from "express";
+import { User } from "../database/user/model";
+
+interface User {
+  body: {
+    name: string;
+    email: string;
+    password: string;
+  };
+};
+
+export const create = async ({ body }: User, response: Response) => {
+  const { name, email, password } = body;
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  return response.status(201).json(user).end();
+};
