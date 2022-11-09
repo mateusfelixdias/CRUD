d6a7a11052876ec00b23a89baf66894eef8e6782
mateusfelixdias <felixmateus618@gmail.com>
2022-11-09 16:24:57 -0300

    config: server configuration

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import { routes } from "./routes/router";
+import { connection } from "./database/connection";
+
+const app = express();
+
+connection();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("server running on port 3000!");
+});
