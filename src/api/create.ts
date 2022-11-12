9c3c81851eafe96ae91e9d18c18cd51f9d4d2f69
mateusfelixdias <felixmateus618@gmail.com>
2022-11-12 10:23:54 -0300

    feat: added validation on user creation

--- src/api/create.ts
+++ src/api/create.ts
@@ -1,22 +1,34 @@
 import { Response } from 'express';
+import { Body } from '../interfaces/body';
 import { User } from '../database/user/model';
+import { User as TypeUser } from '../interfaces/user';
+import { hashInThePassword } from '../utils/hash/hash';
+import { validationCreateUser } from '../utils/verify/validationCreateUser';
 
-interface User {
-  body: {
-    name: string;
-    email: string;
-    password: string;
-  };
-}
+export const create = async ({ body }: Body, response: Response) => {
+  const { name, email, password }: TypeUser = body;
 
-export const create = async ({ body }: User, response: Response) => {
-  const { name, email, password } = body;
-
-  const user = await User.create({
+  const validationFieldsCreationUser = await validationCreateUser({
     name,
     email,
     password,
   });
 
-  return response.status(201).json(user).end();
+  if (validationFieldsCreationUser === true) {
+    if (await User.findOne({ email })) {
+      return response.status(400).json('E-mail já está cadastrado!').end();
+    };
+
+    const hash = hashInThePassword(password);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hash,
+    });
+
+    return response.status(201).json(user).end();
+  };
+
+  return response.status(400).json(validationFieldsCreationUser);
 };
