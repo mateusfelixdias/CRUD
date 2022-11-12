15d6065b8fda534aba886cbf842df9b49fc6bc66
mateusfelixdias <felixmateus618@gmail.com>
2022-11-12 14:39:18 -0300

    feat: added read callback functions update and delete

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,17 @@
-import { create } from '../api/create';
 import express from 'express';
+import { read } from '../api/read';
+import { create } from '../api/create';
+import { destroy } from '../api/delete';
+import { update } from '../api/update';
 
 const routes = express.Router();
 
 routes.post('/users', create);
 
-routes.get('/users');
+routes.get('/users', read);
 
-routes.put('/users/:id');
+routes.put('/users/:id', update);
 
-routes.delete('/users/:id');
+routes.delete('/users/:id', destroy);
 
 export { routes };
