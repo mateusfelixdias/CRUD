c210d719c9166e5f07b131df05c97428590f1cf3
mateusfelixdias <felixmateus618@gmail.com>
2022-11-12 10:25:49 -0300

    feat: added unique in the email field

--- src/database/user/model.ts
+++ src/database/user/model.ts
@@ -9,7 +9,7 @@ interface IUser {
 const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, require: true },
   },
   { timestamps: true }
